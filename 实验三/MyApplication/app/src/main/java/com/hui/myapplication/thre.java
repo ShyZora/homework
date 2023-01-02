bcae54ac0028dfef31dc1529b72a259329e276d2
zfxdshsq <2253354434@qq.com>
2023-01-02 21:43:19 +0800

    tesy

---
+++ 实验三/MyApplication/app/src/main/java/com/hui/myapplication/thre.java
@@ -0,0 +1,12 @@
+package com.hui.myapplication;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class thre extends AppCompatActivity { @Override
+protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.home);
+}
+}
